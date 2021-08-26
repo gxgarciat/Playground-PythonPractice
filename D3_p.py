bb2712497456ead8aedf8b7532253d89056cc9a2
gxgarciat <info@gxgarciat.dev>
2021-08-26 11:14:44 +0100

    Coding practice

---
+++ D3_p.py
@@ -0,0 +1,46 @@
+# Type a program that will accept a two digits number and adds their digits together
+# For example, if the input is 35, the output should be 3 + 5 = 8
+
+# Input Block
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+# Operation block
+initialDirection = input("Do you want to go left or right \n")
+if initialDirection == "Right" or initialDirection == "right" :
+    print("Game Over")
+else:
+    meanTransport = input("Do you want to go swim or wait \n")
+    if meanTransport == "Swim" or meanTransport == "swim" :
+        print("Game Over")
+    else:
+        colorDoor = input("Choose between the blue, red or the yellow door \n")
+        if colorDoor == "blue" or colorDoor == "red":
+            print("Game Over")
+        else:
+            print("You win!")
+
+
+# Output block
