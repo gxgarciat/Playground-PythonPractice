0d3c78e1210a550adfb3a92307e5615e54052328
gxgarciat <info@gxgarciat.dev>
2021-08-29 09:18:59 +0100

    Coding practice

---
+++ D4_c2.py
@@ -0,0 +1,17 @@
+# Build a virtual coin toss program that will randomly tell the user "Heads" or "Tails"
+# The first letter should be capitalised and spelt exactly like in the example
+
+import random
+
+# Input block
+name_string = input("Give me everybody's names, separated by a comma. \n")
+names = name_string.split(", ")
+
+print(names)
+
+# Operations block
+
+# Output block
+print("\n****************************************")
+print(f"        You got .")
+print("****************************************")
