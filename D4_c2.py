32cd9324a880608eaef27fba194dc656fb33b4d3
gxgarciat <info@gxgarciat.dev>
2021-08-29 21:33:24 +0100

    Refactoring

--- D4_c2.py
+++ D4_c2.py
@@ -1,17 +1,19 @@
-# Build a virtual coin toss program that will randomly tell the user "Heads" or "Tails"
-# The first letter should be capitalised and spelt exactly like in the example
+# Build a program which will select a random name from a list of names
+# The person selected will have to pay for everybody's food bill
 
 import random
+winnerBill = 0
 
 # Input block
 name_string = input("Give me everybody's names, separated by a comma. \n")
 names = name_string.split(", ")
 
-print(names)
-
 # Operations block
+numberPart = len(names)
+
+winnerBill = random.randint(0,numberPart-1)
 
 # Output block
-print("\n****************************************")
-print(f"        You got .")
-print("****************************************")
+print("\n**************************************************")
+print(f"  We have a winner! {names[winnerBill]} will pay the bill!")
+print("*****************************************************")
