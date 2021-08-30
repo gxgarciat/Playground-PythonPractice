9e404b3c1eda58d1622a5dc72bb96a6d0d60c40f
gxgarciat <info@gxgarciat.dev>
2021-08-30 13:02:02 +0100

    Coding practice

---
+++ D4_c3.py
@@ -0,0 +1,25 @@
+# Write a program which will mark a spot with an X
+# This program contains a nested list, as per the instruction
+# Replace the grey box with an X, according to the coordinates
+
+# Input block
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? \n")
+
+# Operations block
+horizontal = int(position[0])
+vertical = int(position[1])
+
+selectedRow = map[vertical -1]
+selectedRow[horizontal-1] = "X"
+
+# Output block
+print("\n**************************************************")
+print(f"    Your treasure is located as follows!")
+print("**************************************************")
+
+print(f"{row1}\n{row2}\n{row3}")
