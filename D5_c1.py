fca0e52b036880a84bd5acd04856910c52e5c309
gxgarciat <info@gxgarciat.dev>
2021-09-18 20:34:10 +0100

    Refactoring

--- D5_c1.py
+++ D5_c1.py
@@ -7,14 +7,11 @@ totalPopulation = 0
 averageHeight = 0
 i = 0
 
-
 # Input block
 student_heights = input("Input a list of student heights: \n").split()
 
-
 for n in range(0, len(student_heights)):
   student_heights[n] = int(student_heights[n])
-
 
 # Operations block
 # By using defined functions
