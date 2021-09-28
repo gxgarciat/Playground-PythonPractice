3c0c1723c66fefba81fae45788374b9a2022765e
gxgarciat <info@gxgarciat.dev>
2021-09-28 22:39:34 +0100

    Refactoring

--- D5_c1.py
+++ D5_c1.py
@@ -2,22 +2,27 @@
 # student_heights = [180, 124, 165, 173, 189, 169, 146]
 # The average height can be calculated by adding all the heights together and dividing by the total number of heights.
 
+
 totalSum = 0
 totalPopulation = 0
 averageHeight = 0
 i = 0
 
+
 # Input block
 student_heights = input("Input a list of student heights: \n").split()
 
+
 for n in range(0, len(student_heights)):
   student_heights[n] = int(student_heights[n])
+
 
 # Operations block
 # By using defined functions
 # totalSum = sum(student_heights)
 # totalPopulation = len(student_heights)
 # averageHeight = round(totalSum/totalPopulation)
+
 
 # By using FOR loop
 #print(student_heights,type(student_heights))
